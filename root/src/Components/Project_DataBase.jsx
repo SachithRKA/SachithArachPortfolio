04074cd069629a2a951433588007a4dec76d6db6
SachithRKA <147006220+SachithRKA@users.noreply.github.com>
2024-01-15 23:21:48 -0500

    Portfolio Update

---
+++ root/src/Components/Project_DataBase.jsx
@@ -0,0 +1,16 @@
+const Project_DataBase = [
+    {
+        Project_Name: "Architect Website",
+        skills: "HTML / CSS / JS",
+        Img_src: "./img/Projects/weather_API.png",
+        Github_link: "https://github.com/SachithRKA/Weather-API"
+    },
+    {
+        Project_Name: "Budget App",
+        skills: "HTML / CSS / JS",
+        Img_src:  "./img/Projects/weather_API.png",
+        Github_link: "https://github.com/SachithRKA/Weather-API"
+    }
+]
+ 
+export default Project_DataBase; 
---
+++ root/src/Components/Skill_List.jsx
@@ -0,0 +1,29 @@
+let Skill_List = [
+    {
+        title: "Languages",
+        skill: ["Python", "JavaScript", "Java"]
+    },
+ 
+    {
+        title: "Frameworks",
+        skill: ["Node.js", "Express.js", "React.js"]
+    },
+ 
+    {
+        title: "Databases",
+        skill: ["PostgreSQL"]
+    },
+ 
+    {
+        title: "Web Technologies",
+        skill: ["HTML", "CSS", "Bootstrap"]
+    },
+ 
+    {
+        title: "Tools",
+        skill: ["Git/GitHub", "Linux", "Bash", "REST API", "HTTP Protocols", "CRUD"]
+    }
+ ];
+ 
+ export default Skill_List;
+ 
