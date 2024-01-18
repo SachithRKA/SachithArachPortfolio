f4afdc2e9570f5f0f0e234d6ee6246493acf2254
SachithRKA <147006220+SachithRKA@users.noreply.github.com>
2024-01-17 22:12:50 -0500

    Updated project sections

--- src/Components/Project_DataBase.jsx
+++ src/Components/Project_DataBase.jsx
@@ -1,15 +1,27 @@
 const Project_DataBase = [
     {
-        Project_Name: "Architect Website",
-        skills: "HTML / CSS / JS",
-        Img_src: "./img/Projects/weather_API.png",
+        Project_Name: "Family Library",
+        skills: "PostgreSQL / HTML / SQL / CSS / JS",
+        Img_src: "./img/Projects/Family-Library.png",
+        Github_link: "https://github.com/SachithRKA/FamilyLibrary"
+    },
+    {
+        Project_Name: "Weather API",
+        skills: "API / HTTP / REST API / Node.js / EJS",
+        Img_src: "./img/Projects/WeatherbitAPI.png",
         Github_link: "https://github.com/SachithRKA/Weather-API"
     },
     {
-        Project_Name: "Budget App",
-        skills: "HTML / CSS / JS",
-        Img_src:  "./img/Projects/weather_API.png",
-        Github_link: "https://github.com/SachithRKA/Weather-API"
+        Project_Name: "MyBlog",
+        skills: "EJS / JS / Express / Node / CRUD",
+        Img_src: "./img/Projects/YourBlog.png",
+        Github_link: "https://github.com/SachithRKA/MyBlog"
+    },
+    {
+        Project_Name: "Sachith Arach Portfolio",
+        skills: "HTML / CSS / SCSS / REACT / JS",
+        Img_src: "./img/Projects/portfolio.png",
+        Github_link: "https://github.com/SachithRKA/SachithArachPortfolio"
     }
 ]
  
